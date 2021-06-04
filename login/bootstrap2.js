d678ac7eba4e856719a77e64465f9ac1d8d50664
Arpit <111arpit1@gmail.com>
2021-06-04 23:06:36 +0530

    ctf2

---
+++ login/bootstrap2.js
@@ -0,0 +1,17 @@
+
+var username = "Pgexi"
+var passWord = "RpeIjgtIwtUapv"
+
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+
+function check(){{
+    console.log("hello");
+    if(username.value == userName){
+        alert("wjggpn Ndj uxcs xi Rdcvgpijapixdch")
+    }
+    else{
+        alert("Password Not Matched!!")
+    }
+    event.preventDefault()
+}}
